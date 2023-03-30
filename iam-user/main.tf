73155f122ea254943d85d789f41800024ba4bd8c
Hashim Krasniqi <hashim.krasniqi@gmail.com>
2023-03-30 19:04:35 +0200

    Prova Finale

--- iam-user/main.tf
+++ iam-user/main.tf
@@ -24,7 +24,8 @@ data "aws_iam_policy_document" "this" {
       "eks:DescribeCluster",
       "eks:ListClusters",
       "eks:DescribeAddonVersions",
-      "eks:ListIdentityProviderConfigs"
+      "eks:ListIdentityProviderConfigs",
+      "s3:CreateBucket"
     ]
     resources = ["*"]
   }
@@ -49,4 +50,13 @@ resource "aws_iam_user_login_profile" "this" {
 
 resource "aws_iam_access_key" "this" {
   user = aws_iam_user.this.name
+}
+
+resource "aws_s3_bucket" "s3" {
+  bucket = "my-tf-test-bucket-cimi"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Assigment"
+  }
 }
--- iam-user/terraform.tfvars
+++ iam-user/terraform.tfvars
@@ -1,2 +1,2 @@
-access_key = "AKIAUR6LRUFA5Z44QAE3"
-secret_key = "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"
+access_key = "AKIAUR6LRUFA2RJYQIXN"
+secret_key = "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"
